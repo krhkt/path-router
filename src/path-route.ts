6552c188213ef31d13e91d10a5cf4241232f1713
krhkt <89042777+krhkt@users.noreply.github.com>
2022-11-12 23:37:16 -0800

    path route constructor tests

--- src/path-route.ts
+++ src/path-route.ts
@@ -1,66 +1,128 @@
 import { PathRouteHandler} from "./path-route-handler";
 
-const rgPartWithPlaceholder = /^(?<prefix>[^\{\}]*)[\{](?<identifier>[^\{\}]+)[\}](?<suffix>[^\{\}]*)$/;
+// a path template placeholder can have it's identifer surrounded by a prefix and/or a suffix string
+// valid placeholders parts examples:
+//  {identifier}
+//  prefix{identifier}
+//  pf{identifer}suffix
+const rgPartWithPlaceholder = /^(?<prefix>[^\{\}]*)[\{](?<identifier>[^\*\{\}]+)[\}](?<suffix>[^\{\}]*)$/;
+// a greedy placeholder cannot be prefixed or suffixed by strings
+// and has the following format: {*identifier}
+const rgGreedyPlaceholder = /^\{\*(?<identifier>[^\}\{]+)\}$/;
 
-type StringParsedPathPartType = {
+export type StringParsedPathPartType = {
     type: 'string',
     path: string,
 };
 
-type IdentifierParsedPathPartType = {
-    type: 'identifier',
+export type GreedyPlaceholderParsedPathPartType = {
+    type: 'greedy',
+    path: string,
+    identifier: string,
+};
+
+export type ConstraintValidatorType = RegExp | ((input: string) => boolean);
+
+export type PlaceholderParsedPathPartType = {
+    type: 'placeholder',
+    path: string,
     prefix: string,
     identifier: string,
     suffix: string,
-    regex?: RegExp,
+    constraint?: ConstraintValidatorType,
 };
 
-export type ParsedPathPartType = StringParsedPathPartType | IdentifierParsedPathPartType;
+export type ParsedPathPartType =
+    StringParsedPathPartType
+    | GreedyPlaceholderParsedPathPartType
+    | PlaceholderParsedPathPartType;
+
+export type ConstraintType = {
+    [index: string]: ConstraintValidatorType
+};
+
+export type DefaultsType = {
+    [index: string]: string,
+};
 
 export class PathRoute {
-  separator: string;
-  name: string;
-  pathTemplate: string;
-  defaults: object;
-  constraints: object;
-  parsedPathTemplate: Array<ParsedPathPartType> | undefined;
-  //routeHandler: PathRouteHandler;
+    separator: string;
+    name: string;
+    pathTemplate: string;
+    defaults: DefaultsType;
+    constraints: ConstraintType;
+    parsedPathTemplate: Array<ParsedPathPartType> | undefined;
+    //routeHandler: PathRouteHandler;
 
-  constructor(
-    name: string,
-    path: string,
-    separator: string,
-    defaults: object | null = null,
-    constraints: object | null = null,
-  ) {
-    this.name = name;
-    this.pathTemplate = path;
-    this.separator = separator;
-    this.defaults = defaults || {};
-    this.constraints = constraints || {};
-  }
+    constructor(
+        name: string,
+        path: string,
+        separator: string,
+        defaults: DefaultsType | null = null,
+        constraints: ConstraintType | null = null,
+    ) {
+        this.name = name;
+        this.pathTemplate = path;
+        this.separator = separator;
+        this.defaults = defaults || {};
+        this.constraints = constraints || {}
 
-  processPathTemplate() {
-    this.parsedPathTemplate = [];
-
-    const templateParts = this.pathTemplate.split(this.separator);
-    for (const part of templateParts) {
-        const matches = rgPartWithPlaceholder.exec(part);
-        const g = matches?.groups!;
-
-        const parsedPathPart: ParsedPathPartType =
-            (matches === null)
-            ? { type: 'string', path: part }
-            : {
-                type: 'identifier',
-                prefix: g.prefix,
-                identifier: g.identifier,
-                suffix: g.suffix,
-            };
-
-        this.parsedPathTemplate.push(parsedPathPart);
+        this.parsedPathTemplate = this.processPathTemplate();
+        this.validateConstraintDefinitions(this.parsedPathTemplate, this.constraints);
     }
 
-    return this.parsedPathTemplate;
-  }
+    processPathTemplate() {
+        const parsedPaths = [];
+
+        const templateParts = this.pathTemplate.split(this.separator);
+        const lastIndex = templateParts.length - 1;
+        for (let i = 0; i < templateParts.length; i += 1) {
+            const part = templateParts[i];
+
+            if (i === lastIndex) {
+                const greedMatch = rgGreedyPlaceholder.exec(part);
+                if (greedMatch !== null) {
+                    parsedPaths.push({
+                        type: 'greedy',
+                        path: part,
+                        identifier: greedMatch.groups!.identifier,
+                    } as GreedyPlaceholderParsedPathPartType);
+                    continue;
+                }
+            }
+            const matches = rgPartWithPlaceholder.exec(part);
+            const g = matches?.groups!;
+
+            const parsedPathPart: ParsedPathPartType =
+                (matches === null)
+                ? { type: 'string', path: part }
+                : {
+                    type: 'placeholder',
+                    path: part,
+                    prefix: g.prefix,
+                    identifier: g.identifier,
+                    suffix: g.suffix,
+                };
+
+            parsedPaths.push(parsedPathPart);
+        }
+
+        return parsedPaths;
+    }
+
+    validateConstraintDefinitions(parsedPathTemplate: Array<ParsedPathPartType>, constraints: ConstraintType) {
+        for (const constraintIdentifier of Object.keys(constraints)) {
+            const existsInPaths = parsedPathTemplate.some((path) => {
+                if (path.type === 'string') return false;
+
+                return path.identifier === constraintIdentifier;
+            });
+
+            if (existsInPaths) continue;
+
+            throw new Error(
+                `"${constraintIdentifier}" doesn't exist as part of the route template path: "${this.pathTemplate}"`
+            );
+        }
+    }
 }
--- test/path-route.test.ts
+++ test/path-route.test.ts
@@ -1,18 +1,118 @@
 import * as assert from 'node:assert';
 import { PathRoute } from '../src/path-route';
+import type { PlaceholderParsedPathPartType } from '../src/path-route';
 
 describe('PathRoute', () => {
     describe('processPathTemplate()', () => {
-        it('process ', () => {
+        it('path without placeholder only generates string parsed parts', () => {
             // arrange
             const route = new PathRoute('main-route', '/test/sample', '/');
+            const expectedResults = ['', 'test', 'sample'];
 
             // act
             const parsedResult = route.processPathTemplate();
 
             // assert
-            assert.equal(parsedResult.length, 3);
+            assert.equal(parsedResult.length, expectedResults.length);
+            for (let i = 0; i < parsedResult.length; i += 1) {
+                const parsedPart = parsedResult[i];
+                assert.equal(parsedPart.path, expectedResults[i]);
+            }
         });
+
+        it('path with placeholder generates an identifier parsed path part', () => {
+            // arrange
+            const route = new PathRoute('main-route', '/basepath/{innerParam}/prefix-{idParam}-suffix', '/');
+            const expectedResults = ['', 'basepath', '{innerParam}', 'prefix-{idParam}-suffix'];
+            const expectedInnerParam = {
+                prefix: '',
+                identifier: 'innerParam',
+                suffix: '',
+            };
+            const expectedIdParam = {
+                prefix: 'prefix-',
+                identifier: 'idParam',
+                suffix: '-suffix',
+            };
+
+            // act
+            const parsedResult = route.processPathTemplate();
+            const identifierPartsResult = [parsedResult[2], parsedResult[3]] as Array<PlaceholderParsedPathPartType>;
+
+            // assert
+            assert.equal(parsedResult.length, expectedResults.length);
+            for (let i = 0; i < parsedResult.length; i += 1) {
+                const parsedPart = parsedResult[i];
+                assert.equal(parsedPart.path, expectedResults[i]);
+            }
+
+            const expectedIdentifiers = [expectedInnerParam, expectedIdParam];
+            for (let i = 0; i < expectedIdentifiers.length; i += 1) {
+                const expected = expectedIdentifiers[i];
+                const actual = identifierPartsResult[i];
+
+                assert.equal(actual.prefix, expected.prefix);
+                assert.equal(actual.identifier, expected.identifier);
+                assert.equal(actual.suffix, expected.suffix);
+            }
+        });
+
+        it('only accepts greedy placeholder as the last element', () => {
+            // arrange
+            const routeA = new PathRoute('routeA', '/base/{identifier}/{*greedyIdentifier}', '/');
+            const expectedPartTypesRouteA = ['string', 'string', 'placeholder', 'greedy'];
+
+            const routeB = new PathRoute('routeB', '/base/{*greedyIdentifier}/more-path', '/');
+            const expectedPartTypesRouteB = ['string', 'string', 'string', 'string'];
+
+            // act
+            const parsedRouteAResults = routeA.processPathTemplate();
+
+            const parsedRouteBResults = routeB.processPathTemplate();
+
+            // assert
+            for (let i = 0; i < parsedRouteAResults.length; i += 1) {
+                assert.equal(parsedRouteAResults[i].type, expectedPartTypesRouteA[i])
+            }
+
+            for (let i = 0; i < parsedRouteBResults.length; i += 1) {
+                assert.equal(parsedRouteBResults[i].type, expectedPartTypesRouteB[i])
+            }
+        });
+    });
+
+    describe('validateConstraintDefinitions()', () => {
+        it(`throws an Error if constraints have identifier not present in the pathTemplate`, () => {
+            // arrange
+            const pathTemplate = '/{firstIdentifier}/path/parts/{secondIdentifier}';
+            const constraints = {firstIdentifier: /\d+/, inexistentIdentifier: /\w+/};
+
+            // act
+            try {
+                const route = new PathRoute('route-name', pathTemplate, '/', null, constraints);
+            } catch (error) {
+                assert.notEqual(error, null);
+                return;
+            }
+
+            assert.fail('invalid constraints should cause error on route instantiation');
+        });
+
+        it(`doesn't throw if all constraints exist in the pathTemplate`, () => {
+            // arrange
+            const pathTemplate = '/basePath/{controller}/{action}/{id}/{*tail}';
+            const constraints = {action: /\w+/, id: /\d+/, tail: /\D+/};
+
+            // act
+            try {
+                const route = new PathRoute('route-name', pathTemplate, '/', null, constraints);
+            } catch (e: any) {
+                console.log(e, e.message);
+                assert.fail('valid constraints should not throw Error on create');
+            }
+
+            assert.ok(true);
+        })
     });
 });
 
